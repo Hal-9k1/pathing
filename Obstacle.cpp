dc717b2834ab42f090aef2fb87f5665db288ca72
Eddy Meals <edimea211@gmail.com>
2025-11-07 23:59:45 -0800

    Struggle through intersectsSegment math

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -5,18 +5,35 @@ Obstacle::Obstacle(const Vec2 &pos, double rot, const Vec2 &size)
 {
   double s = std::sin(rot);
   double c = std::cos(rot);
-  v0 = c * size.x + s * size.y;
-  v1 = -s * size.x + c * size.y;
-  u0 = { c, s };
-  u1 = { -s, c };
-  k0 = v0.dot(v0);
-  k1 = v1.dot(v1);
-  Vec2 u(v0 / 2, v1 / 2);
-  Vec2 v(v0 / 2, -v1 / 2);
-  corners[0] = pos - u;
-  corners[1] = pos + v;
-  corners[2] = pos + u;
-  corners[3] = pos - v;
+  u = c * size.x + s * size.y;
+  v = -s * size.x + c * size.y;
+  Vec2 xy(u / 2, v / 2);
+  Vec2 xny(u / 2, -v / 2);
+  corners[0] = pos - xy;
+  corners[1] = pos + xny;
+  corners[2] = pos + xy;
+  corners[3] = pos - xny;
 }
-bool Obstacle::intersectsSegment(const Segment &segment) const;
-const std::vector<Vec2> &Obstacle::getCorners() const;
+
+bool Obstacle::intersectsSegment(const Segment &segment) const
+{
+  Segment uvSpace = {
+    u.dot(segment.p0) / u.dot(u) * u,
+    v.dot(segment.p1) / v.dot(v) * v
+  };
+  double xa = -uvSpace.p0.x / (uvSpace.p1.x - uvSpace.p0.x);
+  double xb = (2.0 - uvSpace.p0.x) / (uvSpace.p1.x - uvSpace.p0.x);
+  bool xaMin = uvSpace.p1.x > uvSpace.p0.x;
+  double minX = xaMin ? xa ? xb;
+  double maxX = xaMin ? xb ? xa;
+  double ya = -uvSpace.p0.y / (uvSpace.p1.y - uvSpace.p0.y);
+  double yb = (2.0 - uvSpace.p0.y) / (uvSpace.p1.y - uvSpace.p0.y);
+  bool xbMin = uvSpace.p1.y > uvSpace.p0.y;
+  double minY = yaMin ? ya ? yb;
+  double maxY = yaMin ? yb ? ya;
+}
+
+const std::vector<Vec2> &Obstacle::getCorners() const
+{
+  return corners;
+}
